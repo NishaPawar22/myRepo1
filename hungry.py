0a50f77dbffe7d72c3cf3605db6fe6ccc6dc1e33
NishaPawar22 <157485279+NishaPawar22@users.noreply.github.com>
2024-02-01 00:21:29 +0530

    Updated hungry.py

--- hungry.py
+++ hungry.py
@@ -1,5 +1,8 @@
 hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
+    print("eat samosa")
+    print("eat samosa")
 else:
     print("do your homework")
+    print("eat samosa")
