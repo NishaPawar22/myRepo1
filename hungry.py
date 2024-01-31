5cf1fe36be322fa231b0caca1244a782ad31bc3d
Nisha.Pawar <20131093_cse@adcet.in>
2024-02-01 00:17:20 +0530

    added pavbhaji vadapav and cricket

--- hungry.py
+++ hungry.py
@@ -1,5 +1,8 @@
 hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
+    print("eat pavbhaji")
+    print("eat vadapav")
 else:
     print("do your homework")
+    print("play cricket")
