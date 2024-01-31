c6b087636e0eee1397763c97fd8cda3c5d4b2d25
Nisha.Pawar <20131093_cse@adcet.in>
2024-02-01 00:18:26 +0530

    Revert "added pavbhaji vadapav and cricket"
    This reverts commit 5cf1fe36be322fa231b0caca1244a782ad31bc3d.

--- hungry.py
+++ hungry.py
@@ -1,8 +1,5 @@
 hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
-    print("eat pavbhaji")
-    print("eat vadapav")
 else:
     print("do your homework")
-    print("play cricket")
